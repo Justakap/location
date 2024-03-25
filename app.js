1cde99e3e5258b4d5ca535cc48ed43bc1c6dc2d2
Justakap <119529108+Justakap@users.noreply.github.com>
2024-03-25 17:49:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -15,14 +15,15 @@ app.use(bodyParser.json());
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://marklogistics.netlify.app",
         methods: ["GET", "POST"],
         credentials: true
     }
 });
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    // origin: ["http://localhost:3000"],
+    origin: ["https://marklogistics.netlify.app"],
     credentials: true,
 }));
 
